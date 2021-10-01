3df0cd03228e741f8dc6508d7203c00da8629334
ghatikawaykole <ghatika99@gmail.com>
2021-10-01 08:26:20 +0530

    welcome to deck of card

---
+++ src/com/example/deckofcard/DeckOfCard.java
@@ -0,0 +1,10 @@
+package com.example.deckofcard;
+/* purpose deck of cards
+ * create a deck of cards with concept of object oriented program
+ */
+
+public class DeckOfCard {
+    public static void main(String args[]){
+        System.out.println("wlcome to the deck of cards");
+    }
+}
