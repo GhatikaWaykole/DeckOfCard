f86029b13d7fde060e9727c628fe9fce84568fe1
ghatikawaykole <ghatika99@gmail.com>
2021-10-06 12:14:23 +0530

    Added method to compute card as the player

--- src/com/example/deckofcard/Card.java
+++ src/com/example/deckofcard/Card.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Card{
-    ArrayList<String> arrayList = new ArrayList<>();
-    String[] suit = {"Clubs,Diamonds,Hearts,Spade"};
-    String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+    static ArrayList<String> arrayList = new ArrayList<>();
+    static String[] suit = {"Clubs,Diamonds,Hearts,Spade"};
+    static String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
   //  String[][] deckOfCards = {suit,rank};
 
     //craete method to add sit and rank in the arraylist
-    public void deckOfCards(){
+    public static void deckOfCards(){
             for (int i = 0; i< suit.length; i++) {
                 for (int j = 0; j < rank.length; j++) {
                     arrayList.add(suit[i] + " " + rank[j]);
@@ -18,18 +18,31 @@ public class Card{
             }
        display(arrayList);
         }
-    public void display(ArrayList<String> arrayList) {
+    public static void display(ArrayList<String> arrayList) {
         for(String card:arrayList){
             System.out.println(card);
         }
     }
-    public void addPlayer(){
+    /* method to use the player to the compute
+     * Player need more than two to compute the deck of cards
+     */
+    public static void addPlayer(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the player");
         int player = scanner.nextInt();
         if(player >2) {
             System.out.println("Enter the 2 more than player");
+            cardsPlayer(player);
         }
 
+    }
+    /* method to use the player to add the cards
+     * @param player and it will take the cards to the deck of cards
+     */
+    public static void cardsPlayer(int player){
+        System.out.println("Player has cards as below");
+        for(int i=0;i<player;i++){
+            System.out.println("the player has cards"+i);
+        }
     }
 }
